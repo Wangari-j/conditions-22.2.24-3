107b08468fe1470ce24e5655647bf6454944fc29
Wangari-j <wangarijudy@gmail.com>
2024-02-22 05:53:33 +0300

    conditions

---
+++ conditions.py
@@ -0,0 +1,15 @@
+#nested conditions
+#putting an if statement inside another if statement
+
+number = 7
+if number > 0:
+
+    if number == 0:
+      print('Is greater')
+
+    else:
+       print('number is positive.')
+
+else:
+   print('Is less')
+
